f34c3f9354137585d6137755d7596251d79f306f
Prabhjot Sandhu <prabhjotssandhu@gmail.com>
2022-12-27 06:12:44 -0800

    init config

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,61 @@
+-- import mason plugin safely
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+-- import mason-lspconfig plugin safely
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
+-- enable mason
+mason.setup({
+  PATH = "prepend", -- skip causes errors idk why
+})
+
+mason_lspconfig.setup({
+  -- list of servers for mason to install
+  ensure_installed = {
+    "cssls",
+    "cucumber_language_server",
+    "dockerls",
+    "bashls",
+    "eslint",
+    "gopls",
+    "golangci_lint_ls",
+    "grammarly",
+    "graphql",
+    "html",
+    "jsonls",
+    "r_language_server",
+    "rust_analyzer",
+    "sumneko_lua",
+    "tailwindcss",
+    "tsserver",
+    "yamlls",
+    "emmet_ls"m
+    "prettier",
+    "cspell",
+  },
+  -- auto-install configured servers (with lspconfig)
+  automatic_installation = true, -- not the same as ensure_installed
+})
+
+mason_null_ls.setup({
+  -- list of formatters & linters for mason to install
+  ensure_installed = {
+    "prettier", -- ts/js formatter
+    "stylua", -- lua formatter
+    "eslint_d", -- ts/js linter
+  },
+  -- auto-install configured formatters & linters (with null-ls)
+  automatic_installation = true,
+})
