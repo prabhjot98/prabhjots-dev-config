abf346c274655b4a9acfd1d499a198fb03ddc9dd
prabhjot98 <pssandhu@ucdavis.edu>
2024-02-12 11:38:28 -0800

    wezterm: update setup

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,32 +7,26 @@ if wezterm.config_builder then
 end
 local OPACITY = 0.9
 
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "OneDark (base16)"
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
-config.font_size = 18
-config.max_fps = 150
+config.font_size = 16
+config.max_fps = 200
 config.window_background_opacity = OPACITY
 config.window_decorations = "RESIZE"
 config.default_workspace = "home"
-config.animation_fps = 30
+config.animation_fps = 60
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.status_update_interval = 1000
 config.show_new_tab_button_in_tab_bar = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.line_height = 0.9
-config.colors = {
-	tab_bar = {
-		background = "rgba(26, 27, 38, " .. OPACITY .. ")",
-	},
-}
 
 local LEFT_SEPERATOR = wezterm.nerdfonts.ple_left_half_circle_thick
 local RIGHT_SEPERATOR = wezterm.nerdfonts.ple_right_half_circle_thick
 
 local function get_current_working_dir(tab)
-	local current_dir = tab.active_pane.current_working_dir or ""
+	local current_dir = tab.active_pane.current_working_dir.file_path or ""
 	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
 
 	return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
@@ -53,5 +47,29 @@ wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
+
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+
+config.keys = {
+	{
+		key = "'",
+		mods = "CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+smart_splits.apply_to_config(config, {
+	direction_keys = { "h", "j", "k", "l" },
+	-- modifier keys to combine with direction_keys
+	modifiers = {
+		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+	},
+})
 
 return config
